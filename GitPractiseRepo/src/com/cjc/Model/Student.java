e3151087e1a6897d948c682d410697fd10818f5b
malojiMane <149044575+malojiMane@users.noreply.github.com>
2023-11-05 21:26:23 +0530

    By prerana

--- GitPractiseRepo/src/com/cjc/Model/Student.java
+++ GitPractiseRepo/src/com/cjc/Model/Student.java
@@ -1,11 +1,15 @@
 package com.cjc.Model;
 
-public class Student {
+public class Student
+{
+   public void m1()
+    {
+	System.out.println("M1-Method");
+    }
 	
-	
-public static void main(String[] args) {
-		
+    public static void main(String[] args) 
+    {
 	System.out.println("jhsashqjnsasjasjaisjaisjwjiojw");
- }
+    }
 
 }
